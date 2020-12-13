aa729ee00aebd9b75ec16f66e91e0695abc8006e
hyoga-dev <75929898+hyoga-dev@users.noreply.github.com>
2020-12-13 16:46:44 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+    function theFunction() {
+        document.getElementById("contoh").innerHTML = "Nganu";
+        
+    }
+
+    function anu() {
+        document.getElementById("ww").style.color = "red";
+        document.getElementById("ww").style.fontSize = "40px";
+    }
+
+    
